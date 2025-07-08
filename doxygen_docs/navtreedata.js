1bef7e14b7ced1711431d89a1b351fe6d32bf813
hbragge-acn <hbragge-acn@users.noreply.github.com>
2025-07-08 08:58:35 +0000

    deploy: 06a6b86d4c7d93fb13e8791fac1efcd84af3d37b

---
+++ doxygen_docs/navtreedata.js
@@ -0,0 +1,77 @@
+/*
+ @licstart  The following is the entire license notice for the JavaScript code in this file.
+
+ The MIT License (MIT)
+
+ Copyright (C) 1997-2020 by Dimitri van Heesch
+
+ Permission is hereby granted, free of charge, to any person obtaining a copy of this software
+ and associated documentation files (the "Software"), to deal in the Software without restriction,
+ including without limitation the rights to use, copy, modify, merge, publish, distribute,
+ sublicense, and/or sell copies of the Software, and to permit persons to whom the Software is
+ furnished to do so, subject to the following conditions:
+
+ The above copyright notice and this permission notice shall be included in all copies or
+ substantial portions of the Software.
+
+ THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, INCLUDING
+ BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND
+ NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM,
+ DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+
+ @licend  The above is the entire license notice for the JavaScript code in this file
+*/
+var NAVTREE =
+[
+  [ "Eclipse OpenBSW", "index.html", [
+    [ "Deprecated List", "da/d58/deprecated.html", null ],
+    [ "Namespaces", "namespaces.html", [
+      [ "Namespace List", "namespaces.html", "namespaces_dup" ],
+      [ "Namespace Members", "namespacemembers.html", [
+        [ "All", "namespacemembers.html", null ],
+        [ "Functions", "namespacemembers_func.html", null ],
+        [ "Enumerations", "namespacemembers_enum.html", null ]
+      ] ]
+    ] ],
+    [ "Classes", "annotated.html", [
+      [ "Class List", "annotated.html", "annotated_dup" ],
+      [ "Class Index", "classes.html", null ],
+      [ "Class Hierarchy", "hierarchy.html", "hierarchy" ],
+      [ "Class Members", "functions.html", [
+        [ "All", "functions.html", "functions_dup" ],
+        [ "Functions", "functions_func.html", "functions_func" ],
+        [ "Variables", "functions_vars.html", null ],
+        [ "Typedefs", "functions_type.html", null ],
+        [ "Enumerations", "functions_enum.html", null ],
+        [ "Enumerator", "functions_eval.html", null ],
+        [ "Related Functions", "functions_rela.html", null ]
+      ] ]
+    ] ],
+    [ "Files", "files.html", [
+      [ "File List", "files.html", "files_dup" ]
+    ] ]
+  ] ]
+];
+
+var NAVTREEINDEX =
+[
+"annotated.html",
+"d1/d69/classuds_1_1DiagnosticSessionControl.html#ab80cd800e9cb057c3ce7acf25a6f045b",
+"d2/d21/classuds_1_1IAsyncDiagHelper_1_1StoredRequest.html#aacf68dbb52a4ecabfb60d605525732ac",
+"d3/d59/DoCanMessageTransmitProtocolHandler_8h.html#aef0e587c0b8e582e261d1f379f147375a74acd6ae34604cdcae5d5766e1f91978",
+"d4/dad/classdocan_1_1IDoCanFlowControlFrameTransmitter.html#a2ac72d3bc5a0d76863dfcee85ce49290",
+"d6/d03/IDoCanFrameReceiver_8h_source.html",
+"d7/da2/classuds_1_1NestedDiagRequest.html#abe5fcea6176421a89ea00323bb7989a3",
+"d8/dd0/classutil_1_1stream_1_1NullOutputStream.html#ae13b9d33b7a48b92f9bd750494cf1886",
+"d9/dbd/classlogger_1_1IBufferedLoggerOutputClient.html#a09aa0626b554b54f3f40a4a8c39e47d4",
+"da/de4/classos_1_1IFutureSupport.html#a0cefa7fb043425ed29b5ee3ae43d12d7",
+"db/dc5/classdocan_1_1declare_1_1DoCanPhysicalCanTransceiverContainerBuilder.html#a29ba772aa7e59644ac4c23fe4fe4d7f8",
+"dc/dbf/classutil_1_1stream_1_1INonBlockingInputStream.html#a752cd9d57f7a1f78e64dadacd5dab5d6",
+"dd/d6a/classasync_1_1Queue.html#aec45cfd4ee263266003bcdf27214e65b",
+"de/d90/classcommon_1_1ITimeoutManager2.html#a8218b96e5e8e8bfd02e79ad7bdb00996ad88763e43cc38134429df8d9fe27cc00",
+"df/d9f/classruntime_1_1StatisticsIterator.html"
+];
+
+var SYNCONMSG = 'click to disable panel synchronisation';
+var SYNCOFFMSG = 'click to enable panel synchronisation';
