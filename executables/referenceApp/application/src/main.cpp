20919704c088cefe9db31777cb5b3341a7e44066
Christoph Rüthing <christoph.cr.ruething@bmw.de>
2025-10-16 12:41:01 +0200

    Configure ETL Asserts for Unit Test and RefApp

--- executables/referenceApp/application/src/main.cpp
+++ executables/referenceApp/application/src/main.cpp
@@ -2,4 +2,33 @@
 
 #include "app/app.h"
 
-void app_main() { ::app::run(); }
+#include <etl/error_handler.h>
+
+#if defined(__linux__) || defined(__APPLE__)
+#include <cstdlib>
+#include <iostream>
+#if __has_include("execinfo.h")
+#include <execinfo.h>
+#endif
+
+void etl_assert_function(etl::exception const& exception)
+{
+#if __has_include("execinfo.h")
+    void* callstack[128];
+    backtrace_symbols_fd(callstack, backtrace(callstack, 128), 2 /* = stderr */);
+#endif
+    std::cout << "Assertion at " << exception.file_name() << ':' << exception.line_number() << " ("
+              << exception.what() << ") failed" << std::endl;
+    std::abort();
+}
+#else
+#include <cstdlib>
+
+void etl_assert_function(etl::exception const&) { std::abort(); }
+#endif
+
+void app_main()
+{
+    etl::set_assert_function(etl_assert_function);
+    ::app::run();
+}
