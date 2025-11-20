cf31c3b1a58caa3f032b71a1bd53b35ed11e61ea
hbragge-acn <hbragge-acn@users.noreply.github.com>
2025-11-20 16:09:38 +0000

    deploy: f326bb97f72fa8eef8c277fdabd2452401a97770

---
+++ doxygen_docs/navtreedata.js
@@ -0,0 +1,82 @@
+/*
+ @licstart  The following is the entire license notice for the JavaScript code in this file.
+
+ The MIT License (MIT)
+
+ Copyright (C) 1997-2020 by Dimitri van Heesch
+
+ Permission is hereby granted, free of charge, to any person obtaining a copy of this software
+ and associated documentation files (the "Software"), to deal in the Software without restriction,
+ including without limitation the rights to use, copy, modify, merge, publish, distribute,
+ sublicense, and/or sell copies of the Software, and to permit persons to whom the Software is
+ furnished to do so, subject to the following conditions:
+
+ The above copyright notice and this permission notice shall be included in all copies or
+ substantial portions of the Software.
+
+ THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, INCLUDING
+ BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND
+ NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM,
+ DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+
+ @licend  The above is the entire license notice for the JavaScript code in this file
+*/
+var NAVTREE =
+[
+  [ "Eclipse OpenBSW", "index.html", [
+    [ "Deprecated List", "da/d58/deprecated.html", null ],
+    [ "Namespaces", "namespaces.html", [
+      [ "Namespace List", "namespaces.html", "namespaces_dup" ],
+      [ "Namespace Members", "namespacemembers.html", [
+        [ "All", "namespacemembers.html", null ],
+        [ "Functions", "namespacemembers_func.html", null ],
+        [ "Enumerations", "namespacemembers_enum.html", null ]
+      ] ]
+    ] ],
+    [ "Classes", "annotated.html", [
+      [ "Class List", "annotated.html", "annotated_dup" ],
+      [ "Class Index", "classes.html", null ],
+      [ "Class Hierarchy", "hierarchy.html", "hierarchy" ],
+      [ "Class Members", "functions.html", [
+        [ "All", "functions.html", "functions_dup" ],
+        [ "Functions", "functions_func.html", "functions_func" ],
+        [ "Variables", "functions_vars.html", null ],
+        [ "Typedefs", "functions_type.html", null ],
+        [ "Enumerations", "functions_enum.html", null ],
+        [ "Enumerator", "functions_eval.html", null ],
+        [ "Related Functions", "functions_rela.html", null ]
+      ] ]
+    ] ],
+    [ "Files", "files.html", [
+      [ "File List", "files.html", "files_dup" ]
+    ] ]
+  ] ]
+];
+
+var NAVTREEINDEX =
+[
+"annotated.html",
+"d1/d47/classudp_1_1LwipDatagramSocket.html#ada54d3ca5652c6e8eed551313123b961",
+"d2/d00/structdocan_1_1DoCanDataLinkLayer.html#a6df9f95d4e88a243a241ef1763b68a48",
+"d2/dd0/structstorage_1_1StorageJob_1_1Type_1_1None.html",
+"d3/d99/namespaceuds.html#a560875185cb8ddb373aaef7cd5ecb6c0afb4d813be6b812f136eaa8496eb103b1",
+"d4/d7a/classutil_1_1format_1_1Vt100AttributedStringFormatter_1_1ApplyAttributes.html",
+"d5/d91/structutil_1_1format_1_1PlainStringAttributes.html#acea77260de9c2228b4bc5807c8c91f7a",
+"d6/d86/classcan_1_1SocketCanTransceiver.html#a8923d6b5d8206dc0460e6ee6aae4bd32",
+"d7/dab/classethernet_1_1TapEthernetDriver.html#a2c0ebccbcafdb4fa0c6d7f35d1ba553a",
+"d8/d66/classuds_1_1StopRoutine.html#a783d6d904c0b084f19dde4bc555744e0",
+"d9/d59/classuds_1_1IAsyncDiagHelper.html#a732e8c3f61ec6dbb24cbd1729d1664bc",
+"da/d36/IPAddress_8h.html#abdf4361cff774da8b3fd3fb4722327fe",
+"da/de1/classudp_1_1AbstractDatagramSocket.html#aa5e545d610b857687701bac3f17375e1",
+"db/d95/classlogger_1_1ComponentConfig.html#a98365957092e12c07ef758d8110ca753",
+"dc/d20/Call_8h_source.html",
+"dc/dbf/classutil_1_1stream_1_1INonBlockingInputStream.html#ae19b5391c28580adbdafbdf5ed650b93",
+"dd/d8c/classtransport_1_1TransportMessage.html#af378013c64e175b13948f882ba5753cda7a5d551d2d392486c7d81933e764f909",
+"de/d90/classcommon_1_1ITimeoutManager2.html#a31fa1a1aa672f620ca1d1368cfb11dd9",
+"df/d86/classbios_1_1CanPhy.html",
+""
+];
+
+var SYNCONMSG = 'click to disable panel synchronisation';
+var SYNCOFFMSG = 'click to enable panel synchronisation';
